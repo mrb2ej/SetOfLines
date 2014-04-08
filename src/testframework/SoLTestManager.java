e0f83d7c1af29a2b3bb4993d00c55891d8006f79
Matt Bongiovi <mrb2ej@virginia.edu>
2014-04-07 22:47:32 -0400

    Minor updates to test framework

--- src/testframework/SoLTestManager.java
+++ src/testframework/SoLTestManager.java
@@ -1,5 +1,6 @@
 package testframework;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import setoflines.Point;
@@ -7,38 +8,49 @@ import setoflines.SetOfLines;
 
 public class SoLTestManager {
 
-	
 	public static void main(String[] args) {
-		
-		TestLog testlog = new TestLog();
-		
-		// Generate a random point set 
-		ArrayList<Point> pointset = null;
-		
-		// Select epsilon error based on point set 
-		double epsilon = 0;
-		
-		// Define a dimension for the point set
-		int dimension = 2;
-		
-		
-		long startTime = System.currentTimeMillis();		
-		SetOfLines setoflines = new SetOfLines(pointset, epsilon, dimension);		
-		long endTime = System.currentTimeMillis();
-		
-		// Check time to compress 
-		long time_to_compress = endTime - startTime;
-		
-		// TODO: How to check compression ratio for the set of lines
-		double compression_ratio = 0;
-		
-		// Log the compression statistics 
-		testlog.log("Time to Compress: " + time_to_compress);
-		testlog.log("Compression Ratio: " + compression_ratio);
-		
-		
-		
 
+		TestLog testlog = new TestLog();
+		int num_tests = 1000;
+		
+		for (int i = 0; i < num_tests; i++){
+			// Generate a random point set
+			ArrayList<Point> pointset = generate_random_pointset();
+
+			// Select epsilon error based on point set
+			double epsilon = 0;
+
+			// Define a dimension for the point set
+			int dimension = 2;
+
+			long startTime = System.currentTimeMillis();
+			SetOfLines setoflines = new SetOfLines(pointset, epsilon, dimension);
+			long endTime = System.currentTimeMillis();
+
+			// Check time to compress
+			long time_to_compress = endTime - startTime;
+
+			// TODO: How to check compression ratio for the set of lines
+			double compression_ratio = 0;
+
+			// Log the compression statistics
+			testlog.log("Test " + (i+1) + " of " + num_tests);
+			testlog.log("-------------------------");
+			testlog.log("Time to Compress: " + time_to_compress);
+			testlog.log("Compression Ratio: " + compression_ratio);
+			testlog.log("\n");
+		}
+		
+		try {
+			testlog.serializeToFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private static ArrayList<Point> generate_random_pointset(){
+		return null;
 	}
 
 }
